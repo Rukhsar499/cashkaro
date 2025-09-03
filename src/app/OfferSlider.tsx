ed025ab44e70f846aae8d9bc30839ff1783b0620
Rukhsar499 <rukshar.digitalwolf@gmail.com>
2025-09-03 13:00:39 +0530

    hii

--- src/app/OfferSlider.tsx
+++ src/app/OfferSlider.tsx
@@ -3,40 +3,73 @@ import Image from "next/image";
 
 export default function OfferSlider() {
     return (
-        <div id="offerCarousel" className="carousel slide" data-bs-ride="carousel">
+        <div id="offerCarousel" className="carousel slide mb first-sec" data-bs-ride="carousel">
             <div className="carousel-inner">
 
                 {/* Slide 1 */}
                 <div className="carousel-item active">
-                    <Image
-                        src="/assets/img/banner.png"
-                        width={600}
-                        height={300}
-                        className="d-block  rounded-3"
-                        alt="Offer 1"
-                    />
+                    <div className="row">
+                        <div className="col-12 col-md-4">
+                            <Image
+                                src="/assets/img/banner.png"
+                                width={600}
+                                height={300}
+                                className="d-block w-100 rounded-3"
+                                alt="Offer 1"
+                            />
+                        </div>
+                        <div className="col-12 col-md-4 d-none d-md-block">
+                            <Image
+                                src="/assets/img/banner2.png"
+                                width={600}
+                                height={300}
+                                className="d-block w-100 rounded-3"
+                                alt="Offer 2"
+                            />
+                        </div>
+                        <div className="col-12 col-md-4 d-none d-md-block">
+                            <Image
+                                src="/assets/img/banner3.png"
+                                width={600}
+                                height={300}
+                                className="d-block w-100 rounded-3"
+                                alt="Offer 3"
+                            />
+                        </div>
+                    </div>
                 </div>
 
                 {/* Slide 2 */}
                 <div className="carousel-item">
-                    <Image
-                        src="/assets/img/banner2.png"
-                        width={600}
-                        height={300}
-                        className="d-block  rounded-3"
-                        alt="Offer 1"
-                    />
-                </div>
-
-                {/* Slide 3 */}
-                <div className="carousel-item">
-                    <Image
-                        src="/assets/img/banner3.png"
-                        width={600}
-                        height={300}
-                        className="d-block  rounded-3"
-                        alt="Offer 1"
-                    />
+                    <div className="row">
+                        <div className="col-12 col-md-4">
+                            <Image
+                                src="/assets/img/banner4.png"
+                                width={600}
+                                height={300}
+                                className="d-block w-100 rounded-3"
+                                alt="Offer 4"
+                            />
+                        </div>
+                        <div className="col-12 col-md-4 d-none d-md-block">
+                            <Image
+                                src="/assets/img/banner5.png"
+                                width={600}
+                                height={300}
+                                className="d-block w-100 rounded-3"
+                                alt="Offer 5"
+                            />
+                        </div>
+                        <div className="col-12 col-md-4 d-none d-md-block">
+                            <Image
+                                src="/assets/img/banner6.png"
+                                width={600}
+                                height={300}
+                                className="d-block w-100 rounded-3"
+                                alt="Offer 6"
+                            />
+                        </div>
+                    </div>
                 </div>
             </div>
 
