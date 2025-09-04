7c7078644a9d59d6cf8c0f79de70a74107beb7ee
Rukhsar499 <rukshar.digitalwolf@gmail.com>
2025-09-04 17:37:20 +0530

    hii

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@
 
 import Header from "../app/component/Header";
 import OfferSlider from "../app/OfferSlider";
+import TopCategoriesSlider from "../app/TopCategoriesSlider";
 
 export default function Home() {
   return (
     <>
     <Header />
     <OfferSlider />
+    <TopCategoriesSlider />
     </>
   );
 }
